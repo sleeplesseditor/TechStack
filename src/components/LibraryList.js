6bd13589f0ecda38e717227432695027721c559a
sleeplesseditor <josh@bryson.me.uk>
2018-04-27 00:35:55 +0100

    Initial ListView setup

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
-    render() {
+    componentWillMount() {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2 
+        });
+
+        this.dataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow() {
         
-        return;
+    }
+
+    render() {
+        return(
+            <ListView 
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
