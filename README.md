d058f949b9f9d69e87f95b938abe0eab3235b762
sleeplesseditor <josh@bryson.me.uk>
2018-04-27 19:24:53 +0100

    App finalised

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # TechStack
 
-An early experiment in the use of Redux with React Native.
+An early experiment in the use of Redux with React Native. The app presents a list of topics associated with React, and when the user selects one, a description drops down (with spring animation).
 
 It uses components from my earlier [ReactNativeAuth project](https://github.com/sleeplesseditor/ReactNativeAuth).
+
+
+![screen shot 2018-04-27 at 19 20 53](https://user-images.githubusercontent.com/25869284/39378447-583e48a6-4a50-11e8-99dc-159d8bdc4c56.png)
+
+App landing screen
+
+
+![screen shot 2018-04-27 at 19 21 03](https://user-images.githubusercontent.com/25869284/39378464-632bf60a-4a50-11e8-98dd-be79fc4cab9c.png)
+
+App with heading selected and description visible.
---
+++ src/components/ListItem.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { 
+    LayoutAnimation,
+    Text, 
+    TouchableWithoutFeedback, 
+    View } from 'react-native';
+import { connect } from 'react-redux';
+import { CardSection } from './common';
+import * as actions from '../actions';
+
+class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }   
+
+    renderDescription() {
+        const { descriptionStyle } = styles;
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={descriptionStyle}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            );
+        }
+    }
+
+    render() {
+        const { titleStyle } = styles;
+        const { id, title } = this.props.library;
+
+        return (
+                <TouchableWithoutFeedback
+                    onPress={() => this.props.selectLibrary(id)}
+                >
+                    <View>
+                        <CardSection>
+                            <Text style={titleStyle}>
+                                {title}
+                            </Text>
+                        </CardSection>
+                        {this.renderDescription()}
+                    </View>
+                </TouchableWithoutFeedback>
+        );
+    }
+}
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
+    },
+    descriptionStyle: {
+        flex: 1,
+        paddingLeft: 15,
+        paddingRight: 15
+    }
+};
+
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
